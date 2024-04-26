e78ef22802e69d85acca55210dcb7eb34ab60574
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-26 18:30:47 +0500

    Now start with App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,20 @@ function App() {
    <>
    <main>
     <div id="game-container">
-
+    <ol id="players">
+     <li>
+      <span className="player">
+      <span className="player-name">Player 1</span>
+      <span className="player-symbol">X</span>
+      </span>
+      <span><button>Edit</button></span>
+     </li>
+     <li>
+      <span className="player-name">Player 2</span>
+      <span className="player-symbol">O</span>
+     </li>
+    </ol>
+    Game Board
     </div>
    </main>
    </>
