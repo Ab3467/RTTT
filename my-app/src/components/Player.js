990e5acb36b125d2c34dab3d56ebeee98b49c525
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-26 18:37:25 +0500

    changes

---
+++ my-app/src/components/Player.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Player() {
+  return (
+    <div>
+       <li>
+      <span className="player">
+      <span className="player-name">Player 1</span>
+      <span className="player-symbol">X</span>
+      </span>
+      <span><button>Edit</button></span>
+     </li>
+     <li></li>
+    </div>
+  )
+}
