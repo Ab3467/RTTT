1b54dfcd52e4d54626b251673d0b1cb0c6c55635
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-26 20:24:42 +0500

    changes

--- my-app/src/components/GameBoard.js
+++ my-app/src/components/GameBoard.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Borad = [
     [null,null,null],
     [null,null,null],
     [null,null,null]
 ]
-
-
 export default function GameBoard() {
+const [symbol,setSymbol]= useState('X') 
+const BtnControl=()=>{
+    setSymbol(symbol ? 'O': 'X');
+}
+
   return (
     <ol id='game-board'>
       {Borad.map((row,rowIndex)=> <li key={rowIndex}>
@@ -15,8 +18,8 @@ export default function GameBoard() {
       <ol>
         {row.map((plySymbol,colIndex)=>
         <li key={colIndex}>
-            <button>
-            {plySymbol}
+            <button onClick={BtnControl}>
+            {symbol}
             </button>
         </li>
         )}
